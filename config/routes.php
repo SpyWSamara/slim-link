5b055cad3335273f3611a3cee45ea58785df7dde
SpyWSamara <spywsamara@gmail.com>
2020-08-19 00:47:51 +0400

    Add creation and handle actions

--- config/routes.php
+++ config/routes.php
@@ -4,4 +4,6 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class)->setName('home');
+    $app->post('/new', \App\Action\NewLinkAction::class)->setName('new-link');
+    $app->get('/{slug:[\w\-]+}', \App\Action\HandleLinkAction::class)->setName('handle-link');
 };
