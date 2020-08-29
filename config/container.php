f11517b8dd0448e66e5c2a9bfc2ff528d2169ffc
SpyWSamara <spywsamara@gmail.com>
2020-08-30 00:02:26 +0400

    Setup conteiner to user class for settings
    Can set settings to other classes by autowiring.

--- config/container.php
+++ config/container.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Settings;
 use Psr\Container\ContainerInterface;
 use Slim\App;
 use Slim\Factory\AppFactory;
 use Slim\Middleware\ErrorMiddleware;
 
 return [
-    'settings' => function () {
-        return require __DIR__.'/settings.php';
+    Settings::class => function (ContainerInterface $container) {
+        return new Settings(require __DIR__.'/settings.php');
     },
 
     App::class => function (ContainerInterface $container) {
@@ -18,14 +19,14 @@ return [
 
     ErrorMiddleware::class => function (ContainerInterface $container) {
         $app = $container->get(App::class);
-        $settings = $container->get('settings')['error'];
+        $settings = $container->get(Settings::class);
 
         return new ErrorMiddleware(
             $app->getCallableResolver(),
             $app->getResponseFactory(),
-            (bool) $settings['display_error_details'],
-            (bool) $settings['log_errors'],
-            (bool) $settings['log_error_details']
+            (bool) $settings['error.display_error_details'],
+            (bool) $settings['error.log_errors'],
+            (bool) $settings['error.log_error_details']
         );
     },
 ];
